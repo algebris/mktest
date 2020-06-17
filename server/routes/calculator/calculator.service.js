2e9a2d3700daa89d1df8b5abc66cbd85bc17df8f
vyacheslav frolov <algebris@gmail.com>
2020-06-17 22:37:44 +0700

    add decimal

--- server/routes/calculator/calculator.service.js
+++ server/routes/calculator/calculator.service.js
@@ -1,24 +1,34 @@
+const Decimal = require('decimal.js');
+
 module.exports.performOperation = (req) => {
   const { previous, last, operation } = req.payload;
   let result;
-  switch (operation) {
-    case 'minus':
-      result = Number(previous) - Number(last);
-      break;
-    case 'plus':
-      result = Number(previous) + Number(last);
-      break;
-    case 'multiply':
-      result = Number(previous) * Number(last);
-      break;
-    case 'divide':
-      result = Number(previous) / Number(last);
-      break;
-    default:
-      break;
-  }
-  if (Number.isNaN(result) || !Number.isFinite(result)) {
+  const prev = new Decimal(previous);
+  Decimal.set({ precision: 25 });
+  try {
+    switch (operation) {
+      case 'minus':
+        result = prev.minus(last);
+        break;
+      case 'plus':
+        result = prev.plus(last);
+        break;
+      case 'multiply':
+        result = prev.times(last);
+        break;
+      case 'divide':
+        result = prev.div(last);
+        break;
+      default:
+        break;
+    }
+  } catch (err) {
     return { error: true, result: null };
   }
-  return { result };
+
+  if (result.gt(Number.MAX_VALUE) || result.lt(-Number.MAX_VALUE)) {
+    return { error: true, result: null };
+  }
+
+  return { result: result.toString() };
 };
